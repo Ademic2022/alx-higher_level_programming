c76cb63eb4a190576ab07213259aaa2f43a5e8d7
Ademic2022 <adeeyomichael77@gmail.com>
2023-04-04 16:28:19 -0700

    updated locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,10 @@
 """ LockedClass
 """
 
-
 class LockedClass:
     __slots__ = 'first_name'
+
+    def __setattr__(self, key, value):
+        if not hasattr(self, 'first_name') and key != 'first_name':
+            raise AttributeError("Cannot add new attribute to LockedClass")
+        object.__setattr__(self, key, value)
