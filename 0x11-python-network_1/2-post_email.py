302f332b0f83b43b3ad0b6b67c027b64c427a681
Adeeyo Michael <118169702+Ademic2022@users.noreply.github.com>
2023-06-30 06:37:53 -0700

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,16 +5,17 @@
 - takes email as a parameter
 - displays the body of the response
 """
+
+import urllib.request
 import sys
 import urllib.parse
-import urllib.request
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
+
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+    print(body)
